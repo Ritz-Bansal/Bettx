1a70c0a3b055a1eeb5183d836274613494d011f6
unknown <rithvikbansal0501@gmail.com>
2025-10-31 09:30:12 +0530

    Removing comments

--- backend/src/controllers/bet.ts
+++ backend/src/controllers/bet.ts
@@ -4,52 +4,24 @@ import { betInputs } from "../zod/inputs";
 import { vJudge } from "./user";
 
 
-//todo -- search button banano kuiki UI/UX designer chutiya hai
-//todo -- the user must get all his total balance as soon as they logs in their wallet
-//todo -- Test if after migration this works correctly or not
+
 
 interface betInterface {
   walletAdd: string;
   VJudgeUserId: string;
   stake: number;
-  // multiplier: number;
 }
 
 
-//I think I should validate the multiplier also in the BE as people can fuck me with FE sending me multiplier very very easily
+
 export async function bet(req: Request<{}, {}, betInterface>, res: Response) {
-  const walletAdd= req.body.walletAdd; //source wallet Addr i.e, jo bet karra hai
+  const walletAdd= req.body.walletAdd; 
   const VJudgeUserId = req.body.VJudgeUserId;
   const stake = req.body.stake;
 
-  // console.log("WalletAddress: ", walletAdd);
-  // console.log("VJudgeUserId: ", VJudgeUserId);
-  // console.log("Stake: ", stake);
-  // let multiplier = req.body.multiplier; //TODO remove it
-
-  // const ranks = await odds();
-  // let odds = 1;
-  // const ranks = await vJudge();
-  // ranks.map((rank) => {
-  //   odds += 0.1;
-  //   const odd = odds.toFixed(2);
-  //   rank.odds = odd;
-  // });
-  
-  // console.log("Ranks using the odd function inside bet to get the multiplier: ", ranks)
-  // ranks.map((data) => {
-  //   if(data.name == VJudgeUserId){
-  //     // multiplier = parseFloat(data.odds); 
-  //     // console.log("Inside the if condition i.e, confirming the multiplier: ", multiplier);
-  //   }
-  // })
-
-
-  // console.log("Chekcing if the value of the multiplier is updated or not: ", multiplier);
-  // multiplier = parseFloat(multiplier);
   const InputsResponse = betInputs.safeParse({
     amount: stake,
-    // multiplier: multiplier,
+
   });
 
   if (InputsResponse.success) {
@@ -79,16 +51,14 @@ export async function bet(req: Request<{}, {}, betInterface>, res: Response) {
 
       let remainingBal: number = totalBalance - totalBetAmount;
 
-      // console.log("Balance: ", totalBalance);
-      // console.log("Total Bet Amount: ", totalBetAmount);
-      // console.log("Remaining Balance bedore the bet is placed: ", remainingBal);
+
 
       if (remainingBal >= stake) {
         const response = await prisma.bet.create({
           data: {
             VjudgeUserId: VJudgeUserId,
             stake: stake,
-            multiplier: 1.0, //remove this bro
+            multiplier: 1.0, 
             walletAddress: walletAdd,
           },
         });
@@ -97,8 +67,8 @@ export async function bet(req: Request<{}, {}, betInterface>, res: Response) {
 
         return res.json({
           message: "Bet successfully placed",
-          balance: remainingBal, // send the remaining balance after the BET is placed,
-          // stake: stake        // Dicey if bhejna hai ki nai
+          balance: remainingBal, 
+
         });
       } else {
         return res.json({
@@ -106,14 +76,14 @@ export async function bet(req: Request<{}, {}, betInterface>, res: Response) {
         });
       }
     } catch (error) {
-      // console.log(error);
+
       return res.json({
         message: "Error placing bet: ",
         error,
       });
     }
   } else {
-    // console.log(InputsResponse.error.issues[0].message);
+
     return res.json({
       message: InputsResponse.error.issues[0].message,
     });
@@ -121,7 +91,7 @@ export async function bet(req: Request<{}, {}, betInterface>, res: Response) {
 }
 
 interface PaymentInterface {
-  // pay: string;
+
   walletAdd: string;
   PlayerNameTheyBettedOn: string;
 }
@@ -133,16 +103,12 @@ interface SplitInterface {
   payEachWinnerInSOL: number;
 }
 
-//contest Id deni padegi of the current contest --> correct aara hai bhai
+
 export async function payment(req: Request, res: Response) {
   
   let payment: PaymentInterface[] = [];
   let split: SplitInterface[] = [];
   const ranks = await vJudge();
-  // console.log(ranks);
-  // console.log("ranks of 0th index", ranks[0]);
-
-  //this will find all the bets placed for the contest
   const allBets = await prisma.bet.findMany();
   
   let totaPool: number = 0;
@@ -156,28 +122,18 @@ export async function payment(req: Request, res: Response) {
   const totalMoneyToPaySecond: number = (30/100)*totalMonetToPay;
   const totalMoneyToPayThird: number = (20/100)*totalMonetToPay;
   const MoneyToSplitArray = [totalMoneyToPayFirst, totalMoneyToPaySecond, totalMoneyToPayThird];
-  // let totalMoneyStakedByRithvik = 0;
-
-  // Only rank1, rank2 and rank3 ko payment jayegi bro --> not this 
-  // of the total amount, remove 10% and split the remaining to 1st, 2nd and 3rd
 
   for (let i = 0; i < 3; i++) {
     let playerToSplitAcross: number = 0;
     const participantName: string = ranks[i].name;
-    // I cannot put toFixed(2) as this is SOL, people will put in decimal only --< this has 13 decimals and Solana hai 9 so life is good
-    allBets.map((data) => {
-      // const money = data.stake
-      // totalMoneyStakedByRithvik += money;
+
+    allBets.map((data: any) => {
       if (data.VjudgeUserId == participantName) {
-        // const pay = data.stake * data.multiplier;        
-        // console.log("Money before adding in the total Money pot: ", pay.toFixed(2));
-        // totalMonetToPay += pay;
         playerToSplitAcross++;
 
         payment.push({
-          // pay: `$${pay}`,
           walletAdd: data.walletAddress,
-          PlayerNameTheyBettedOn: data.VjudgeUserId, // Rakho mat rakho no farak bro
+          PlayerNameTheyBettedOn: data.VjudgeUserId,
         });
       }
     });
@@ -191,20 +147,17 @@ export async function payment(req: Request, res: Response) {
   }
 
   const TotalAmountBettedInSOl = await trial();
-  // console.log("Logs inside the payment function inside the bet controller");
-  // console.log("Total money to pay in SOL: ",totalMonetToPay);
-  // console.log("Total money to pay in USD: ", totalMonetToPay*239);
 
   res.json({
     totoalAmountBettedInSOL: TotalAmountBettedInSOl,
     totalMonetToPayInSOL: totalMonetToPay,
     splitInSOL: split,
     dump: payment,
-    // totalMoneyStakedByRithvik: totalMoneyStakedByRithvik*239
+
   });
 }
 
-//amount in SOL --> total amout betted in SOL -- correct hai yeh 
+
 async function trial() {
   const allbets = await prisma.bet.findMany({})
   let SOL: number = 0;
@@ -213,47 +166,6 @@ async function trial() {
     SOL += allbets[i].stake;
   }
 
-  // console.log("Logs inside the trial function inside the bet controller");
-  // console.log("Amount betted in SOL: ", SOL);
-  // console.log("Amount betted in dollars: ", SOL*239);
+
   return SOL;
 }
-
-
-//1.21
-//0.24
-//0.845
-
-
-//Good for V2, but not for V1
-// export async function addWalletAddress(req,res){
-//     const vJudgeUsername = req.body.vJudgeUsername;
-//     const walletAddressParticipant = req.body.walletAddressParticipant;
-
-//     try{
-//       const allBets = await prisma.bet.updateMany({
-//         where: {
-//           VjudgeUserId: vJudgeUsername
-//         },
-//         data: {
-//           walletAddressParticipant: walletAddressParticipant
-//         }
-//       })
-
-//       // allBets.map(bet => {
-//       //   bet.walletAddressParticipant  = walletAddressParticipant
-//       // })
-
-//       // console.log(allBets[0]);
-
-//     return res.json({
-//       message: "Wallet Address successfully added"
-//     })
-
-//     }catch(error){
-//       return res.json({
-//         message: "Error while adding wallet address ", error
-//       })
-//     }
-
-// }
